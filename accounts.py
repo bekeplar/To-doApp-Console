f1d8aa023b8b207458814c6c18a4a4bbe6a41944
bekeplar <bekeplar@gmail.com>
2018-09-19 14:05:22 -0700

    ft(create-account):create account [delivers-#160614371]

---
+++ accounts.py
@@ -0,0 +1,19 @@
+accounts = {}
+
+
+def add_account(name, password):
+    """Function enables user to create an account"""
+    if not name.isalpha():
+        print("A name should only constist alphabetical characters!")
+    else:
+        accounts[password] = name
+        print( "{} is registered. Proceed and login".format(accounts[password]))
+        return accounts
+
+
+def login(name, password):
+    """Function enables user to login into their account"""
+    for key in accounts:
+        if key == accounts[password]:
+            return True
+     return False
