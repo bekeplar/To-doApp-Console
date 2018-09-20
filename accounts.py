a86c0e7a1b8c6b88c91a9c8f23e049ede3488f7a
bekeplar <bekeplar@gmail.com>
2018-09-19 18:56:53 -0700

    ft(to-do-account):tracking account [Deliver-#160614371]

--- accounts.py
+++ accounts.py
@@ -4,7 +4,7 @@ accounts = {}
 def add_account(name, password):
     """Function enables user to create an account"""
     if not name.isalpha():
-        print("A name should only constist alphabetical characters!")
+        print("A name should only consist alphabetical characters!")
     else:
         accounts[password] = name
         print( "{} is registered. Proceed and login".format(accounts[password]))
@@ -17,4 +17,4 @@ def login(name, password):
         if key == accounts[password]:
             return True
             return False
-     #Accepting credentials
+    """ Accepting credentials"""
