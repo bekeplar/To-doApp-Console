f257d172032aa332abcf5dfac5f088b762eca578
bekeplar <bekeplar@gmail.com>
2018-09-22 11:54:22 -0700

    ft(login):Login feature added [Delivers-#160614416]

--- accounts.py
+++ accounts.py
@@ -7,4 +7,16 @@ def add_account(name, password):
         return "A name should only consist alphabetical characters!",False
 
         accounts[password] = name
-       
+
+def login(name, password):
+    """
+    Returns True if the name and the corresponding password 
+    exists in the accounts dictionary
+    """
+
+    if accounts.get(password):
+        if accounts[password] is name:
+            return True
+        return False
+    return False       
+    """"verrifying the user's credentials""" 
